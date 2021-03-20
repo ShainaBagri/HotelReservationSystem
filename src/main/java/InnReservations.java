8d95b524628dd3d2f97320e7346094d5cf03d732
pipfeld <starstruckcountry@gmail.com>
2021-03-19 17:23:37 -0700

    new liveshare

--- src/main/java/InnReservations.java
+++ src/main/java/InnReservations.java
@@ -12,9 +12,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 
-/*
- * Introductory JDBC examples based loosely on the BAKERY dataset from CSC 365 labs.
- */
 public class InnReservations {
 
 	private final String JDBC_URL = "jdbc:h2:~/csc365_lab7";
@@ -27,6 +24,7 @@ public class InnReservations {
 			lab7.initDb();
 			lab7.getUserInput();
 		} catch (SQLException e) {
+			System.err.println("Error in Main");
 			System.err.println("SQLException: " + e.getMessage());
 		}
 	}
@@ -35,12 +33,21 @@ public class InnReservations {
 		Scanner scanner = new Scanner(System.in);
 		Boolean quit = false;
 		while (!quit) {
-			System.out.println("Enter a number from the following:\n" + "1. Get Rooms and Reservations\n");
+			System.out.println(
+					"Enter a number from the following:\n1. Get Rooms and Reservations\n4. Cancel Reservation\n5. Get Revenue Summary\n");
 			String input = scanner.nextLine();
 			if (input.equals("1")) {
 				getRooms();
+			} else if (input.equals("4")) {
+				cancelReservation();
+			} else if (input.equals("5")) {
+				getSummary();
 			} else if (input.equals("Quit")) {
 				quit = true;
+			} else if (input.equals("6")) {
+				getReservations();
+			} else {
+				System.out.println("Input was not a valid command.");
 			}
 		}
 	}
@@ -49,7 +56,8 @@ public class InnReservations {
 	private void getRooms() {
 		try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
 			try (Statement stmt = conn.createStatement()) {
-				ResultSet rs = stmt.executeQuery("SELECT * FROM lab7_rooms");
+				String query = "SELECT * FROM lab7_rooms";
+				ResultSet rs = stmt.executeQuery(query);
 				while (rs.next()) {
 					String rc = rs.getString("RoomCode");
 					String rn = rs.getString("RoomName");
@@ -63,10 +71,10 @@ public class InnReservations {
 				}
 				System.out.println("----------------------\n");
 			} catch (SQLException e) {
-				System.out.println("Error.");
+				System.err.println("Error.");
 			}
 		} catch (SQLException e) {
-			System.out.println("Errorrrrr");
+			System.err.println("Errorrrrr");
 		}
 	}
 
@@ -89,16 +97,52 @@ public class InnReservations {
 				int kids = scanner.nextInt();
 				System.out.println("Enter the number of adults: ");
 				int adults = scanner.nextInt();
-				ResultSet rs = stmt.executeQuery("SELECT * FROM lab7_rooms WHERE Room='");
+				ResultSet rs = stmt.executeQuery("SELECT * FROM lab7_rooms WHERE Room=?");
 				while (rs.next()) {
 
 				}
 				System.out.println("----------------------\n");
 			} catch (SQLException e) {
-				System.out.println("Error.");
+				System.err.println("Error.");
 			}
 		} catch (SQLException e) {
-			System.out.println("Errorrrrr");
+			System.err.println("Errorrrrr");
+		}
+	}
+
+	// FR4
+	private void cancelReservation() {
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter reservation code: ");
+			Integer code = Integer.valueOf(scanner.nextLine());
+			System.out.println("Are you sure you want to cancel your reservation? Type 0 for No, 1 for Yes: ");
+			String confirm = scanner.nextLine();
+
+			if (confirm.equals("0")) {
+				System.out.println("Cancellation Aborted");
+				return;
+			}
+
+			StringBuilder sb = new StringBuilder("DELETE FROM lab7_reservations WHERE CODE = ?");
+
+			try (PreparedStatement pstmt = conn.prepareStatement(sb.toString())) {
+				pstmt.setObject(1, code);
+
+				try {
+					pstmt.executeUpdate();
+					System.out.println("Cancellation Successful");
+				} catch (SQLException e) {
+					System.err.println("Error Updating SQL Statement");
+					System.err.println("SQLException: " + e.getMessage());
+				}
+			} catch (SQLException e) {
+				System.err.println("Error Creating SQL Statement");
+				System.err.println("SQLException: " + e.getMessage());
+			}
+		} catch (SQLException e) {
+			System.err.println("Error Connecting to JDBC Driver");
+			System.err.println("SQLException: " + e.getMessage());
 		}
 	}
 
@@ -106,37 +150,35 @@ public class InnReservations {
 	private void getSummary() {
 		try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
 			try (Statement stmt = conn.createStatement()) {
-				String query = "";
-				/*
-				 * WITH monthRev AS (SELECT Room, Month, SUM(Price) as monthRevenue FROM( SELECT
-				 * CODE, Room, MONTHNAME(CheckOut) as Month, DATEDIFF(Checkout, CheckIn) * Rate
-				 * as Price FROM reservations ) as theTable GROUP BY Room, Month ORDER BY Room),
-				 * totRev AS(SELECT Room, SUM(monthRevenue) as totalRevenue FROM( SELECT Room,
-				 * Month, SUM(Price) as monthRevenue FROM( SELECT CODE, Room,
-				 * MONTHNAME(CheckOut) as Month, DATEDIFF(Checkout, CheckIn) * Rate as Price
-				 * FROM reservations ) as otherTable GROUP BY Room, Month ORDER BY Room ) as
-				 * thirdTable GROUP BY Room)
-				 * 
-				 * SELECT * FROM totRev NATURAL JOIN monthRev
-				 */
+
+				String monthRev = "SELECT Room, Month, SUM(Price) AS monthRevenue " + "FROM ("
+						+ " SELECT CODE, Room, MONTHNAME(CheckOut) AS Month, DATEDIFF(day, CheckIn, Checkout) * Rate AS Price"
+						+ " FROM lab7_reservations " + ") AS theTable" + " GROUP BY Room, Month" + " ORDER BY Room";
+				String totRev = "SELECT Room, SUM(monthRevenue) AS totalRevenue" + " FROM ("
+						+ " SELECT Room, Month, SUM(Price) AS monthRevenue" + " FROM ("
+						+ "SELECT CODE, Room, MONTHNAME(CheckOut) AS Month, DATEDIFF(day, CheckIn, Checkout) * Rate AS Price"
+						+ " FROM lab7_reservations" + ") AS otherTable" + " GROUP BY Room, Month" + " ORDER BY Room"
+						+ ") AS thirdTable GROUP BY Room";
+				String query = "SELECT A.Room, Month, monthRevenue, totalRevenue FROM (" + totRev + ") A NATURAL JOIN ("
+						+ monthRev + ") B";
 				ResultSet rs = stmt.executeQuery(query);
+
 				while (rs.next()) {
-					String rc = rs.getString("RoomCode");
-					String rn = rs.getString("RoomName");
-					int beds = rs.getInt("Beds");
-					String bedType = rs.getString("bedType");
-					int maxOcc = rs.getInt("maxOcc");
-					float basePrice = rs.getFloat("basePrice");
-					String decor = rs.getString("decor");
-					System.out.println(
-							rc + ' ' + rn + ' ' + beds + ' ' + bedType + ' ' + maxOcc + ' ' + basePrice + ' ' + decor);
+					String room = rs.getString("Room");
+					String month = rs.getString("Month");
+					Float monthRevenue = rs.getFloat("monthRevenue");
+
+					System.out.println(room + ' ' + month + ' ' + monthRevenue);
 				}
 				System.out.println("----------------------\n");
+
 			} catch (SQLException e) {
-				System.out.println("Error.");
+				System.err.println("Error Creating SQL Statement");
+				System.err.println("SQLException: " + e.getMessage());
 			}
 		} catch (SQLException e) {
-			System.out.println("Errorrrrr");
+			System.err.println("Error Connecting to JDBC Driver");
+			System.err.println("SQLException: " + e.getMessage());
 		}
 	}
 
@@ -334,12 +376,26 @@ public class InnReservations {
 				stmt.execute(
 						"CREATE TABLE lab7_reservations (CODE int(11) PRIMARY KEY, Room char(5), CheckIn DATE, Checkout DATE, Rate float, LastName varchar(15), FirstName varchar(15), Adults int(11), Kids int(11), FOREIGN KEY (Room) REFERENCES lab7_rooms (RoomCode))");
 				stmt.execute(
-						"INSERT INTO lab7_rooms (RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) VALUES ('HBB', 'Hello Barbie Beds', 2, 'Queen', 4, 100.0, 'traditional')");
+						"INSERT INTO lab7_rooms (RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) VALUES ('HBB', 'Hello Barbie Beds', 2, 'Queen', 4, 150.0, 'traditional')");
+				stmt.execute(
+						"INSERT INTO lab7_rooms (RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) VALUES ('ABC', 'Alphabet Room', 1, 'Full', 2, 115.0, 'modern')");
+				stmt.execute(
+						"INSERT INTO lab7_rooms (RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor) VALUES ('JAZ', 'Jazz Room', 3, 'Full', 5, 175.0, 'modern')");
 
 				stmt.execute(
-						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (10105, 'HBB', '2010-10-23', '2010-10-25', 100, 'SELBIG', 'CONRAD', 1, 0)");
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (10105, 'HBB', '2010-10-23', '2010-10-25', 155, 'SELBIG', 'CONRAD', 1, 0)");
 				stmt.execute(
-						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (10111, 'HBB', '2010-10-28', '2010-10-30', 100, 'GREENBERG', 'TROY', 1, 0)");
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (10111, 'HBB', '2010-10-28', '2010-10-30', 170, 'GREENBERG', 'TROY', 1, 0)");
+				stmt.execute(
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (12345, 'HBB', '2010-11-01', '2010-11-22', 150, 'KATE', 'MARY', 2, 1)");
+				stmt.execute(
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (23456, 'HBB', '2010-01-02', '2010-01-17', 150, 'SMITH', 'JOY', 1, 2)");
+				stmt.execute(
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (34567, 'HBB', '2010-11-01', '2010-11-22', 150, 'BERN', 'JEREMY', 1, 1)");
+				stmt.execute(
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (13333, 'ABC', '2010-11-01', '2010-11-22', 150, 'BERNER', 'BOB', 1, 0)");
+				stmt.execute(
+						"INSERT INTO lab7_reservations (CODE, Room, CheckIn, Checkout, Rate, LastName, FirstName, Adults, Kids) VALUES (45623, 'ABC', '2010-07-07', '2010-07-23', 125, 'LEE', 'ROY', 1, 0)");
 
 				ResultSet rs = stmt.executeQuery("SELECT * FROM lab7_rooms");
 				while (rs.next()) {
@@ -355,6 +411,33 @@ public class InnReservations {
 							rc + ' ' + rn + ' ' + beds + ' ' + bedType + ' ' + maxOcc + ' ' + basePrice + ' ' + decor);
 				}
 			}
+		}
+	}
+
+	private void getReservations() {
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)) {
+			try (Statement stmt = conn.createStatement()) {
+				ResultSet rs = stmt.executeQuery("SELECT * FROM lab7_reservations");
+				while (rs.next()) {
+					Integer code = rs.getInt("CODE");
+					String room = rs.getString("Room");
+					String checkin = rs.getString("CheckIn");
+					String checkout = rs.getString("Checkout");
+					int rate = rs.getInt("Rate");
+					String lastname = rs.getString("LastName");
+					String firstname = rs.getString("FirstName");
+					int adults = rs.getInt("Adults");
+					int kids = rs.getInt("Kids");
+					System.out.print(code + ' ' + room + ' ' + checkin + ' ' + checkout + ' ' + rate + ' ' + lastname
+							+ ' ' + firstname + ' ' + adults + ' ' + kids);
+				}
+			} catch (SQLException e) {
+				System.err.println("Error Creating SQL Statement");
+				System.err.println("SQLException: " + e.getMessage());
+			}
+		} catch (SQLException e) {
+			System.err.println("Error Connecting to JDBC Driver");
+			System.err.println("SQLException: " + e.getMessage());
 		}
 	}
 
